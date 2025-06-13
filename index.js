abe194c9d150b1004457efd546ce9681d230d96d
Sean <214511560+FernbrookTB@users.noreply.github.com>
2025-06-13 12:09:42 -0500

    Added functionality to the buttons and added results and score from the button clicks

---
+++ index.js
@@ -0,0 +1,73 @@
+let playerScore=0
+let computerScore=0
+let roundCount = 0
+const maxRounds = 5;
+
+function getComputerChoice() {
+    const choice = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choice.length);
+    return choice[randomIndex]
+}
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Draw!'
+    }
+
+    if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            return 'Winner!'
+        } else {
+            return 'Damn, loser..'
+        }
+}
+
+function handlePlayerChoice (playerSelection) {
+    if (roundCount >= maxRounds) return;
+
+    const computerSelection = getComputerChoice();
+    const result = playRound (playerSelection, computerSelection);
+    document.getElementById('result').textContent= `Round ${roundCount + 1}: You chose ${playerSelection}, computer chose ${computerSelection}. ${result}`;
+    
+    
+    
+    if (result === 'Winner!') {
+        playerScore++;
+        roundCount++;
+    } else if (result === 'Damn, loser..') {
+        computerScore++;
+        roundCount++;
+    }
+
+    
+    
+    document.getElementById('playerScore').textContent = `You: ${playerScore}`;
+    document.getElementById('computerScore').textContent = `Computer: ${computerScore}`;
+
+    if (roundCount === maxRounds) {
+        let finalMessage = '';
+        if (playerScore > computerScore){
+            finalMessage = 'You win the match!';
+        } else if (playerScore < computerScore) {
+            finalMessage = 'The Computer bested you.';
+        } else {
+            finalMessage = 'Its a tie!';
+        }
+        document.getElementById('result').textContent += `\n${finalMessage}`;
+    }
+}
+
+document.getElementById('rock').addEventListener('click', () => {
+    handlePlayerChoice('rock')
+});
+
+document.getElementById('paper').addEventListener('click', () => {
+    handlePlayerChoice('paper')
+});
+
+document.getElementById('scissors').addEventListener('click', () => {
+    handlePlayerChoice('scissors')
+});
+
